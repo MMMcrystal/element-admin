7287dc101432fca129f9fe1f22703b4f22589ca3
crystal0414 <mhl0414@163.com>
2021-02-15 18:17:20 +0800

    添加Container布局

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,75 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container style="height: 100vh; border: 1px solid #eee">
+      <el-aside width="250px" style="background-color: rgb(238, 241, 246)">
+        <el-scrollbar style="height: 100%">
+          <el-menu router :default-openeds="['1']">
+            <el-submenu index="1">
+              <template slot="title"><i class="el-icon-document"></i> 内容管理</template>
+              <el-menu-item index="/posts/create">新建文章</el-menu-item>
+              <el-menu-item index="/posts/index">文章列表</el-menu-item>
+            </el-submenu>
+          </el-menu>
+        </el-scrollbar>
+      </el-aside>
+
+  
+  <el-container>
+    <el-header style="text-align: right; font-size: 12px">
+      <el-dropdown>
+        <i class="el-icon-setting" style="margin-right: 15px"></i>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>查看</el-dropdown-item>
+          <el-dropdown-item>新增</el-dropdown-item>
+          <el-dropdown-item>删除</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      <span>王小虎</span>
+    </el-header>
+    <el-scrollbar style="height: 100%">
+      <el-main>
+        <el-table :data="tableData">
+          <el-table-column prop="date" label="日期" width="140">
+          </el-table-column>
+          <el-table-column prop="name" label="姓名" width="120">
+          </el-table-column>
+          <el-table-column prop="address" label="地址">
+          </el-table-column>
+        </el-table>
+      </el-main>
+    </el-scrollbar>
+
+  </el-container>
+</el-container>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body,html{
+    padding: 0;
+    margin: 0;
+  }
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    color: #333;
+  }
+
 </style>
+
+<script>
+  export default {
+    data() {
+      const item = {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      };
+      return {
+        tableData: Array(20).fill(item)
+      }
+    }
+  };
+</script>
