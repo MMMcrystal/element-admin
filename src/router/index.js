8fee4f4e84ef433a17c90bd7f01a2ce9d86958a3
crystal0414 <mhl0414@163.com>
2021-02-18 17:12:00 +0800

    新建页面与路由配置跳转

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import CreateArticle from '../views/CreateArticle.vue'
+import ListArticle from '../views/ListArticle.vue'
 
 Vue.use(VueRouter)
 
@@ -8,15 +9,17 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    redirect: '/articles/index'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/articles/create',
+    name: 'create-article',
+    component: CreateArticle
+  },
+  {
+    path: '/articles/index',
+    name: 'list-article',
+    component: ListArticle
   }
 ]
 
