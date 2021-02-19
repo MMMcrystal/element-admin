21a289d0755a18192cf28d2c179a1267f55247c9
crystal0414 <mhl0414@163.com>
2021-02-19 18:03:12 +0800

    配置eslint+prettier优化代码

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,51 @@
 <template>
   <el-container style="height: 100vh; border: 1px solid #eee">
-      <el-aside width="250px" style="background-color: rgb(238, 241, 246)">
-        <el-scrollbar style="height: 100%">
-          <el-menu router :default-openeds="['1']">
-            <el-submenu index="1">
-              <template slot="title"><i class="el-icon-document"></i> 内容管理</template>
-              <el-menu-item index="/articles/index">文章列表</el-menu-item>
-              <el-menu-item index="/articles/create">新建文章</el-menu-item>
-            </el-submenu>
-          </el-menu>
-        </el-scrollbar>
-      </el-aside>
+    <el-aside width="250px" style="background-color: rgb(238, 241, 246)">
+      <el-scrollbar style="height: 100%">
+        <el-menu router :default-openeds="['1']">
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-document"></i> 内容管理</template>
+            <el-menu-item index="/articles/index">文章列表</el-menu-item>
+            <el-menu-item index="/articles/create">新建文章</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-scrollbar>
+    </el-aside>
 
-  
-  <el-container>
-    <el-header style="text-align: right; font-size: 12px">
-      <el-dropdown>
-        <i class="el-icon-setting" style="margin-right: 15px"></i>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item>查看</el-dropdown-item>
-          <el-dropdown-item>新增</el-dropdown-item>
-          <el-dropdown-item>删除</el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-      <span>王小虎</span>
-    </el-header>
-    <el-scrollbar style="height: 100%">
-      <el-main>
-        <router-view></router-view>
-      </el-main>
-    </el-scrollbar>
-
+    <el-container>
+      <el-header style="text-align: right; font-size: 12px">
+        <el-dropdown>
+          <i class="el-icon-setting" style="margin-right: 15px"></i>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>查看</el-dropdown-item>
+            <el-dropdown-item>新增</el-dropdown-item>
+            <el-dropdown-item>删除</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+        <span>王小虎</span>
+      </el-header>
+      <el-scrollbar style="height: 100%">
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-scrollbar>
+    </el-container>
   </el-container>
-</el-container>
 </template>
 
 <style>
-  body,html{
-    padding: 0;
-    margin: 0;
-  }
-  .el-header {
-    background-color: #B3C0D1;
-    color: #333;
-    line-height: 60px;
-  }
-  
-  .el-aside {
-    color: #333;
-  }
+body,
+html {
+  padding: 0;
+  margin: 0;
+}
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
 
+.el-aside {
+  color: #333;
+}
 </style>
-
